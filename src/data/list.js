e4da56e833acc84e9ae4db6d51189b4ac86d2f64
JigmeTenzinChogyel <jigmetenzinchogyel@gmail.com>
2024-07-19 14:14:11 +0600

    changed: file import path

--- src/data/list.js
+++ src/data/list.js
@@ -1,29 +1,33 @@
+import books from '../assets/books.png';
+import footballShirt from '../assets/football-shirt.png';
+import apple from '../assets/apple.png';
+import buddy from '../assets/buddy.png';
+
 const list = [
     {
       id: 1,
-      image: 'src/assets/books.png',
+      image: books,
       title: "Small groups in classes",
       description: "Our school has a small classes orientation. There are 7–10 pupils in a class. So all processes are near to private format of education."
     },
     {
       id: 2,
-      image: 'src/assets/football-shirt.png',
+      image: footballShirt,
       title: "Dayle Sport Trainings",
       description: "All our pupils learn several foreign languages. And most classes of a program are curated by native speakers from differen countries."
     },
     {
       id: 3,
-      image: 'src/assets/apple.png',
+      image: apple,
       title: "Healthy and Fresh Food",
       description: "A school has a main base education system and provides a set of subjects to each student for separate learning. These classes are held in the extra time"
     },
     {
       id: 4,
-      image: 'src/assets/buddy.png',
+      image: buddy,
       title: "Love and Hugs",
       description: "If you understand that some additional class is unsuitable, you can try some another to understand your predisposition."
     }
   ];
   
   export default list;
-  
--- src/data/teacherList.js
+++ src/data/teacherList.js
@@ -1,45 +1,52 @@
+import guy1 from '../assets/guy1.jpg';
+import girl1 from '../assets/girl1.jpg';
+import guy2 from '../assets/guy2.jpg';
+import girl2 from '../assets/girl2.jpg';
+import girl3 from '../assets/girl3.jpg';
+import guy3 from '../assets/guy3.jpg';
+
 const teacherList = [
     {
         id: 1,
         name: 'Max Holden',
         subject: 'BIOLOGY CLASSES',
         language: 'Spanish native speaker',
-        image: 'src/assets/guy1.jpg'
+        image: guy1
     },
     {
         id: 2,
         name: 'Melisa Watson',
         subject: 'HISTORY CLASSES ',
         language: '‌English native speaker',
-        image: 'src/assets/girl1.jpg'
+        image: girl1
     },
     {
         id: 3,
         name: 'Joe John',
         subject: 'MATH  CLASSES',
         language: 'English native speaker',
-        image: 'src/assets/guy2.jpg'
+        image: guy2
     },
     {
         id: 4,
         name: 'Antonia Watson',
         subject: 'PHYSICS  CLASSES',
         language: 'German  native speaker',
-        image: 'src/assets/girl2.jpg'
+        image: girl2
     },
     {
         id: 5,
         name: 'Berta Watson',
         subject: 'MUSIC CLASSES',
         language: 'French native speaker',
-        image: 'src/assets/girl3.jpg'
+        image: girl3
     },
     {
         id: 6,
         name: 'Jigme Tenzin Chogyel',
         subject: 'Chemistry CLASSES',
         language: 'Dzongkha native speaker',
-        image: 'src/assets/guy3.jpg'
+        image: guy3
     },
 ]
 
